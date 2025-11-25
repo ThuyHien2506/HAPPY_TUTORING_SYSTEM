3c77cc7ac84291252f0f3caf59f228dfea45bc8a
HieuVan <hieu27718@gmail.com>
2025-11-25 10:51:23 +0700

    Fix error of scheduling backend (bean)

--- backend/src/main/java/com/project/happy/repository/IStudentSchedulingRepository.java
+++ backend/src/main/java/com/project/happy/repository/IStudentSchedulingRepository.java
@@ -2,9 +2,6 @@ package com.project.happy.repository;
 
 import java.time.LocalDateTime;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public interface IStudentSchedulingRepository extends ISchedulingRepository {
     boolean bookAppointment(Long studentId, Long tutorId,
                             LocalDateTime date, LocalDateTime startTime, String topic);
